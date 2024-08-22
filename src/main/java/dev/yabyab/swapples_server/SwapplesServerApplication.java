2c4db3a661e6e94362e9588fa839e2221bc98f6c
Scruffy pc <cayabyabrr@yahoo.com>
2024-08-21 21:00:37 -0700

    init

---
+++ src/main/java/dev/yabyab/swapples_server/SwapplesServerApplication.java
@@ -0,0 +1,13 @@
+package dev.yabyab.swapples_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwapplesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwapplesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yabyab/swapples_server/SwapplesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yabyab.swapples_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwapplesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
