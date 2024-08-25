8d01eee186d7c84f169803ca5ecac6d29a4ac98d
Scruffy pc <cayabyabrr@yahoo.com>
2024-08-24 22:38:37 -0700

    User init

---
+++ src/main/java/dev/yabyab/model/User.java
@@ -0,0 +1,12 @@
+package dev.yabyab.model;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String email;
+    private String profilePictureUrl;
+    private String bio;
+    private int epochTimeCreated;
+//    private String dateOfBirth;
+}
---
+++ src/main/java/dev/yabyab/model/UserStats.java
@@ -0,0 +1,11 @@
+package dev.yabyab.model;
+
+public class UserStats {
+    private int highScore;
+    private int numberOfMatched;
+    private int gamesPlayed;
+    private int totalGamesPlayed;
+    private int totalGamesWon;
+    private int totalGamesLost;
+    private int totalGamesCompleted;
+}
